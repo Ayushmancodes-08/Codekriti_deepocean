07c7f8e4fd1dac00fadfdb94bf8a971f56a64028
Ayushmancodes-08 <patraayushman21@gmail.com>
2026-02-24 17:55:34 +0530

    h

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -111,7 +111,7 @@ const HeroSection = () => {
                             type="button"
                             onClick={() => {
                                 if (window.innerWidth < 640) {
-                                    triggerDownload('/rulebooks/brochure.pdf', 'CodeKriti_4_0_Brochure.pdf');
+                                    triggerDownload('/assets/docs/codekriti_brochure.pdf', 'CodeKriti_4_0_Brochure.pdf');
                                 } else {
                                     smoothScrollTo('about', { duration: 350, easing: 'easeInOutQuart' });
                                 }
