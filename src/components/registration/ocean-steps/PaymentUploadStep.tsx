c7a95a6be81543158ab10f830d00195037547d54
Ayushmancodes-08 <patraayushman21@gmail.com>
2026-02-26 23:18:55 +0530

    fix(payment): update UPI_ID constant in PaymentUploadStep component
    - Changed UPI_ID from "7846803607-2@ybl" to "7008660169-2@ybl" for payment processing
    - Ensured consistency of payment identifier in the registration flow

--- src/components/registration/ocean-steps/PaymentUploadStep.tsx
+++ src/components/registration/ocean-steps/PaymentUploadStep.tsx
@@ -8,7 +8,7 @@ import { Button } from '@/components/ui/button';
 import { OceanFormItem } from '@/components/ui/ocean-form';
 import { cn } from '@/lib/utils';
 
-const UPI_ID = "7846803607-2@ybl";
+const UPI_ID = "7008660169-2@ybl";
 
 interface PaymentUploadStepProps {
     amount: string;
